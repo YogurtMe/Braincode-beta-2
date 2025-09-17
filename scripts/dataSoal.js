e7e5fe39d6cc6db58fc176fb443fce3e569bbe75
YogurtMe <appaaffan6@gmail.com>
2025-09-17 10:50:23 +0700

    Add files via upload

---
+++ scripts/dataSoal.js
@@ -0,0 +1,30 @@
+let dataSoal =
+[
+  {
+    "id": "js-mat-1",
+    "title": "JS Output",
+    "soal": [
+      {
+        "pertanyaan": "apa fungsi console.log pada javascript?",
+        "pilihan": [
+          "Mengambil input",
+          "Melakukan perulangan",
+          "Mengeluarkan output",
+          "Melakukan percabangan"
+        ],
+        "jawaban": 2
+      },
+      {
+        "pertanyaan": "bagaimana penulisan output yang benar pada javascript?",
+        "pilihan": [
+          "log.console = 'Hello, World!'",
+          "console.log = 'Hello, World!'",
+          "output.log('Hello, World!');",
+          "console.log('Hello, World!');"
+        ],
+        "jawaban": 3,
+        "isLast": true
+      }
+    ]
+  }
+]
