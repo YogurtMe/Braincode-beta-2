5b41d75aa3ac06e4d02b65122ed179b80be9bc8e
YogurtMe <appaaffan6@gmail.com>
2025-09-17 10:40:02 +0700

    Add files via upload

---
+++ BrainCode-14-setember (4)/BrainCode/scripts/dataMateri.js
@@ -0,0 +1,29 @@
+let dataMateri =
+[
+  {
+    "id": "js-mat-1",
+    "header": {
+      "title": "judul materi",
+      "subTitle": "deskripsi singkat"
+    },
+    "content": [
+      {
+        "type": "t",
+        "content": "isi title"
+      },
+      {
+        "type": "p",
+        "content": "isi paragraf"
+      },
+      {
+        "type": "c",
+        "title": "Example",
+        "content": [
+          " line 1 ",
+          " line 2 ",
+          " line 3 dan seterusny "
+        ]
+      }
+    ]
+  }
+]
